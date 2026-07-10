80ba32e2b5495401ed6ec522eb93ea4ecf823e5f
Neerjara <55031473+Neerjara@users.noreply.github.com>
2026-07-10 20:52:10 +0530

    Create terraform remote backend.tf

---
+++ terraform remote backend.tf
@@ -0,0 +1,15 @@
+# Resource Group az commands to create remote backend
+az group create \
+    --name nm_group \
+    --location centralindia
+
+# Storage Account
+az storage account create \
+    --name myterraformstate12345 \
+    --resource-group nm_group \
+    --sku Standard_LRS
+
+# Blob Container
+az storage container create \
+    --name tfstate \
+    --account-name myterraformstate12345
